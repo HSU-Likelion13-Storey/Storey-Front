b95e0bd4b165e8a3954b3e5422fb67e7af48f072
조윤지 <yunji8533@naver.com>
2025-08-11 01:27:40 +0900

    ✨: B2C 회원가입 페이지 생성

---
+++ src/pages/B2CSignupPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import SignupCommonForm from '../components/auth/signup/common/SignupCommonForm';
+import { useSignupAccountForm } from '../hooks/useSignupAccountForm';
+
+const B2CSignupPage = () => {
+  const form = useSignupAccountForm();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    // TODO: 유효성 + API 연동
+    console.log('B2C submit', form.values);
+    alert('B2C 가입(더미) 완료');
+  };
+
+  return (
+    <div>
+      <SignupCommonForm
+        values={form.values}
+        setField={form.setField} 
+        handleChange={form.handleChange}
+        requestCode={form.requestCode}
+        verifyCode={form.verifyCode}
+        onSubmit={submit}
+        submitting={form.submitting}
+        submitLabel="가입하기"
+      />
+    </div>
+  );
+};
+
+export default B2CSignupPage;
