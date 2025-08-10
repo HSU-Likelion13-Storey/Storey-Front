8b01fff85864149a4d2492104ef4fe80c4fd733e
조윤지 <yunji8533@naver.com>
2025-08-10 20:46:03 +0900

    ✨: 회원가입 페이지BrandHeading 컴포넌트 구현

---
+++ src/components/auth/signup/common/BrandHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../../../assets/logo-text.svg';
+import './BrandHeading.scss';
+
+export default function BrandHeading({ title }) {
+  return (
+    <div className="brand-heading">
+      <img src={logo} alt="스토어리" className="brand-logo" />
+      <p className="brand-title">{title}</p>
+    </div>
+  );
+}
