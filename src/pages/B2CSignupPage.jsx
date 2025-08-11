b2632be4033e56af2a5a7693f88bc08ac331618e
조윤지 <yunji8533@naver.com>
2025-08-11 16:45:47 +0900

    ✨: B2B, B2C 회원가입 페이지에 완료 화면 라우팅 추가

--- src/pages/B2CSignupPage.jsx
+++ src/pages/B2CSignupPage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import SignupCommonForm from "../components/auth/signup/common/SignupCommonForm";
 import { useSignupAccountForm } from "../hooks/useSignupAccountForm";
+import { useNavigate } from "react-router-dom";
 
 const B2CSignupPage = () => {
   const form = useSignupAccountForm();
+  const nav = useNavigate;
 
   const submit = async (e) => {
     e.preventDefault();
     // TODO: 유효성 + API 연동
-    console.log("B2C submit", form.values);
-    alert("B2C 가입(더미) 완료");
+    console.log("B2C 가입", form.values);
+    nav("/signup/complete");
   };
 
   return (
