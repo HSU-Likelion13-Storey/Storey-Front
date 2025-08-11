98ee8cabc26a618add86759ba7b684edbafa68bf
구혁모 <gurah129@gmail.com>
2025-08-11 22:38:49 +0900

    🍱: 마스코트 그림자 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import logoKakao from "./kakao-logo.svg";
 import logoText from "./logo-text.svg";
 import profileImg from "./profile.svg";
+import logoShadow from "./logo-shadow.svg";
 
-export { logoKakao, logoText, profileImg };
+export { logoKakao, logoText, profileImg, logoShadow };
