776b8af5bafe6aa3bc1d05c9db0dccb871c28a82
구혁모 <gurah129@gmail.com>
2025-08-11 20:18:46 +0900

    ✨: 유저 마이페이지 분기 처리 + 유저 전용 UI 추가

--- src/components/mypage/MyPageScreen.jsx
+++ src/components/mypage/MyPageScreen.jsx
@@ -1,37 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
 import { MdOutlineLogout } from "react-icons/md";
 import { logoText, profileImg } from "@/assets";
 import "./MyPageScreen.scss";
 
 export const MyPageScreen = ({ children }) => {
+  const [role, setRole] = useState("user");
+
   return (
     <div className="container">
-      <header className="header">
+      <header className={`header ${role === "user" && "header-user"}`}>
         <img src={logoText} alt="" />
         <div className="blank"></div>
       </header>
-      <div className="profile-card">
+      <div className={`profile-card ${role === "user" && "profile-user"}`}>
         <div className="profile-content">
           <img src={profileImg} alt="" />
           <span>최사장</span>
         </div>
       </div>
       <div className="list-content">
-        <div className="list-item">
-          <span>구독 상태 확인하기</span>
-          <IoIosArrowForward />
-        </div>
-        <div className="list-item">
-          <span>QR코드 다운 받기</span>
-          <IoIosArrowForward />
-        </div>
-        <div className="list-item logout">
-          <span>로그아웃</span>
-          <MdOutlineLogout />
-        </div>
+        {role === "user" ? (
+          <ListItem text={"나의 캐릭터 도감"} />
+        ) : (
+          <>
+            <ListItem text={"구독 상태 확인하기"} />
+            <ListItem text={"QR코드 다운 받기"} />
+          </>
+        )}
+
+        <ListItem text={"로그아웃"} logout={true}>
+          <MdOutlineLogout className="icon" />
+        </ListItem>
       </div>
       {children}
+    </div>
+  );
+};
+
+const ListItem = ({ children, logout = false, text }) => {
+  return (
+    <div className={`list-item ${logout && "logout"}`}>
+      <span>{text}</span>
+      {children ? children : <IoIosArrowForward className="icon" />}
     </div>
   );
 };
