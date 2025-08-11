73af41fe8765ae3848d524cd83f8427b08534459
조윤지 <yunji8533@naver.com>
2025-08-11 16:36:49 +0900

    ✨: App.jsx에 회원가입 완료페이지 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Layout } from "./components/Layout";
 import LoginPage from "./pages/LoginPage";
 import B2CSignupPage from "./pages/B2CSignupPage";
 import B2BSignupPage from "./pages/B2BSignupPage";
+import SignupComplete from "./components/auth/signup/common/SignupComplete.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup/b2c" element={<B2CSignupPage />} />
           <Route path="/signup/b2b" element={<B2BSignupPage />} />
+          <Route path="/signup/complete" element={<SignupComplete />} />
         </Route>
       </Routes>
     </BrowserRouter>
