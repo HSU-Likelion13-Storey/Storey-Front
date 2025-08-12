d47b4ffd1f81af70a2bf549f1404a2866d6809f1
구혁모 <gurah129@gmail.com>
2025-08-13 02:34:27 +0900

    ✨: 유저 홈 모달 구현

--- src/components/user/Home/UserHome.jsx
+++ src/components/user/Home/UserHome.jsx
@@ -1,8 +1,16 @@
-import { logoText } from "@/assets";
+import { logoText, mascotNone } from "@/assets";
 import "./UserHome.scss";
 import { MapContent } from "./MapContent";
+import { useState } from "react";
 
 export const UserHome = () => {
+  const [modalState, setModalState] = useState({ open: true, visible: true });
+
+  const modalHandle = () => {
+    setModalState((prev) => ({ ...prev, visible: false }));
+    setTimeout(() => setModalState((prev) => ({ ...prev, open: false })), 300);
+  };
+
   return (
     <div className="user-home">
       {/* Header */}
@@ -10,6 +18,22 @@ export const UserHome = () => {
         <img src={logoText} alt="" />
       </header>
       <MapContent />
+      {modalState.open && (
+        <div className={`home-modal ${modalState.visible && "visible"}`}>
+          <div className="modal-content">
+            <div className="message-wrapper">
+              <span className="caption">사장님의 스토리가 담긴 캐릭터를 모아봐요✨</span>
+              <span className="message">
+                <span>가게를 방문해 QR 코드를 찍어 캐릭터를 수집해보세요!</span>
+              </span>
+            </div>
+            <img src={mascotNone} alt="" />
+          </div>
+          <button className="modal-btn" onClick={modalHandle}>
+            네, 알겠습니다!
+          </button>
+        </div>
+      )}
     </div>
   );
 };
