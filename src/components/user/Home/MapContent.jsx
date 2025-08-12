d179a4bd11b03db8da3d98f0d4fb26200ad650e9
구혁모 <gurah129@gmail.com>
2025-08-13 03:10:05 +0900

    📝: 현재 위치 가져오기 주석 처리

--- src/components/user/Home/MapContent.jsx
+++ src/components/user/Home/MapContent.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Map, MarkerClusterer, useKakaoLoader } from "react-kakao-maps-sdk";
 import { logoPig } from "@/assets";
 import { CustomMarker } from "./CustomMarker";
@@ -8,13 +8,16 @@ export const MapContent = () => {
   const [draggable, setDraggable] = useState(true); // 드래그 제어
   const [activeMarker, setActiveMarker] = useState(null); // 클릭된 마커 id/인덱스
   const [isClickMark, setIsClickMark] = useState(false);
+  const [position, setPosition] = useState({ lat: 37.588877588035935, lng: 127.00944853922176 });
   const mapRef = useRef();
+
   // 최신 SDK를 사용하기 위해 script방법이 아닌 방법으로 로더를 호출하는 방법으로 설정
   useKakaoLoader({
     appkey: import.meta.env.VITE_KAKAO_APP_KEY,
     libraries: ["services", "clusterer", "drawing"],
   });
 
+  // 검은 배경 클릭 시 선택된 마커 오버레이 닫기
   const blurHandle = () => {
     setZoomable(true);
     setDraggable(true);
@@ -22,6 +25,7 @@ export const MapContent = () => {
     setIsClickMark(false);
   };
 
+  // 마커 클릭 시
   const markerClickHandle = (index) => {
     setZoomable(false);
     setDraggable(false);
@@ -29,6 +33,7 @@ export const MapContent = () => {
     setIsClickMark(true);
   };
 
+  // 클러스터 지도 레벨 조절 함수
   const onClusterclick = (_target, cluster) => {
     const map = mapRef.current;
     // 현재 지도 레벨에서 1레벨 확대한 레벨
@@ -37,11 +42,37 @@ export const MapContent = () => {
     // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
     map.setLevel(level, { anchor: cluster.getCenter() });
   };
+
+  // TODO 현재 내 위치 가져오기. 일단 사장 등록한 곳으로 보여주기
+  // useEffect(() => {
+  //   if (!navigator.geolocation) {
+  //     console.error("Geolocation API를 지원하지 않는 브라우저입니다.");
+  //     return;
+  //   }
+
+  //   navigator.geolocation.getCurrentPosition(
+  //     (pos) => {
+  //       setPosition({
+  //         lat: pos.coords.latitude,
+  //         lng: pos.coords.longitude,
+  //       });
+  //     },
+  //     (err) => {
+  //       console.error("위치 가져오기 실패:", err);
+  //     },
+  //     {
+  //       enableHighAccuracy: true, // GPS 정확도 높임
+  //       timeout: 5000, // 최대 대기 시간
+  //       maximumAge: 0, // 캐시된 위치 사용 안 함
+  //     },
+  //   );
+  // }, []);
+
   return (
     <div className="map-content">
       {/* 지도. 카카오맵 라이브러리 사용 */}
       <Map
-        center={{ lat: 37.5886230594546, lng: 127.00942586233262 }}
+        center={position}
         style={{ width: "100%", height: "100%" }}
         zoomable={zoomable}
         draggable={draggable}
