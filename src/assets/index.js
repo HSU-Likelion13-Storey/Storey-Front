2e1d90e0e1992e8b1a20c683b33db18b70e11771
구혁모 <gurah129@gmail.com>
2025-08-12 23:32:07 +0900

    🍱: 지도 마커 테스트용 이미지 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import logoText from "./logo-text.svg";
 import profileImg from "./profile.svg";
 import logoShadow from "./logo-shadow.svg";
 import mascotHappy from "./mascot-happy.svg";
+import logoPig from "./pig-logo.svg";
 
-export { logoKakao, logoText, profileImg, logoShadow, mascotHappy };
+export { logoKakao, logoText, profileImg, logoShadow, mascotHappy, logoPig };
