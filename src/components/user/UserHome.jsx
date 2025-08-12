115e714fb98f5e8cc3784bb5907163c5f11b91d9
구혁모 <gurah129@gmail.com>
2025-08-12 20:32:51 +0900

    ✅: 카카오맵 테스트

--- src/components/user/UserHome.jsx
+++ src/components/user/UserHome.jsx
@@ -1,5 +1,6 @@
 import { logoText } from "@/assets";
 import "./UserHome.scss";
+import { CustomOverlayMap, Map } from "react-kakao-maps-sdk";
 
 export const UserHome = () => {
   return (
@@ -8,7 +9,14 @@ export const UserHome = () => {
       <header className={`header`}>
         <img src={logoText} alt="" />
       </header>
-      <div className="map-content">{/* 지도 */}</div>
+      <div className="map-content">
+        {/* 지도 */}
+        <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "100%" }}>
+          <CustomOverlayMap position={{ lat: 33.55635, lng: 126.795841 }}>
+            <div style={{ padding: "42px", backgroundColor: "#fff", color: "#000" }}>Custom Overlay!</div>
+          </CustomOverlayMap>
+        </Map>
+      </div>
     </div>
   );
 };
