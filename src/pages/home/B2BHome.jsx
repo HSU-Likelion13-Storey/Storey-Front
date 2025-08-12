8f2934c7b053f86cc57083488504794702ff1ee8
조윤지 <yunji8533@naver.com>
2025-08-13 00:50:27 +0900

    ✨: B2BHome 페이지 구현

---
+++ src/pages/home/B2BHome.jsx
@@ -0,0 +1,11 @@
+import B2BHomeScreen from "@/components/home/b2b/B2BHomeScreen";
+import { Footer } from "@/components/common/Footer";
+
+export default function B2BHome() {
+  return (
+    <>
+      <B2BHomeScreen />
+      <Footer />
+    </>
+  );
+}
