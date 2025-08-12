8397d25d8026a098cec2bd1b66fa4da80e3cacb0
구혁모 <gurah129@gmail.com>
2025-08-13 03:02:45 +0900

    ⚡️: 커스텀 오버레이 zindex 조절 및 오버레이 분리

--- src/components/user/Home/CustomMarker.jsx
+++ src/components/user/Home/CustomMarker.jsx
@@ -6,20 +6,19 @@ export const CustomMarker = ({ data, isActive, onMarkerClick, blurHandle, isClic
   const [lock, setLock] = useState(true);
 
   return (
-    <CustomOverlayMap position={data.position}>
+    <CustomOverlayMap position={data.position} zIndex={isClickMark && !isActive ? 1 : 100}>
       <div className="marker-container">
         {/* 마커 */}
-        <div className={`marker ${isClickMark && !isActive ? "invisible" : ""}`} onClick={onMarkerClick}>
+        <div className={`marker`} onClick={onMarkerClick}>
           <img src={data.image} className={`logo-img ${lock && "dark"}`} alt="" />
           {lock && <IoIosLock className="icon" />}
-          {/* 오버레이 */}
-          <div className={`overlay ${isActive && "visible"}`}>
-            <div className="title">{data.name}</div>
-            <div className="address">{data.address}</div>
-            <div className="event">{data.event}</div>
-          </div>
         </div>
-
+        {/* 오버레이 */}
+        <div className={`overlay ${isActive && "visible"}`}>
+          <div className="title">{data.name}</div>
+          <div className="address">{data.address}</div>
+          <div className="event">{data.event}</div>
+        </div>
         {/* 블러 처리용 */}
         <div className={`blur ${isActive && "visible"}`} onClick={blurHandle}></div>
       </div>
