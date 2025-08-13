ef8ce4b5935af17ea1c458a5c11e4ac0d81516bd
조윤지 <yunji8533@naver.com>
2025-08-14 04:31:11 +0900

    💄: CharacterBlock 컴포넌트 스타일 구현

--- src/components/home/b2b/CharacterBlock.jsx
+++ src/components/home/b2b/CharacterBlock.jsx
@@ -1,20 +1,26 @@
+import "./CharacterBlock.scss";
 import { useNavigate } from "react-router-dom";
+import { RiPencilFill } from "react-icons/ri";
 
 export default function CharacterBlock({ speech, imageSrc, name, description }) {
   const nav = useNavigate();
+
   return (
     <section className="block">
-      <div className="block-hint">사용자에게 보여지는 멘트로, 수정이 가능해요.</div>
-      <div className="block-bubble">{speech}</div>
       <div className="block-card">
+        <p className="block-hint">사용자에게 보여지는 멘트로, 수정이 가능해요.</p>
+        <div className="block-bubble">{`“${speech}”`}</div>
+
         <img className="block-img" src={imageSrc} alt={name} />
-        <h3 className="block-name">{name}</h3>
+        <p className="block-name">{name}</p>
         <p className="block-desc">{description}</p>
-      </div>
-      <div className="block-edit">
-        <button className="edit-inline" type="button" onClick={() => nav("/character/edit")}>
-          수정하기
-        </button>
+
+        <div className="block-edit">
+          <button type="button" className="button-edit" onClick={() => nav("/home/b2b/edit")}>
+            <RiPencilFill className="button-edit-icon" aria-hidden />
+            <span className="button-edit-text">수정하기</span>
+          </button>
+        </div>
       </div>
     </section>
   );
