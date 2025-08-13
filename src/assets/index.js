132c3d49e5786375bdeb1aed9cc08f78411ad57b
구혁모 <gurah129@gmail.com>
2025-08-13 20:54:26 +0900

    🍱: 테스트용 캐릭터 에셋 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,18 @@ import mascotHappy from "./mascot-happy.svg";
 import mascotNone from "./mascot-none.svg";
 import logoPig from "./pig-logo.svg";
 import qrOverlay from "./qr-overlay.svg";
+import logoTest from "./test-logo.svg";
+import shadowChar from "./character-shadow.svg";
 
-export { logoKakao, logoText, profileImg, logoShadow, mascotHappy, logoPig, mascotNone, qrOverlay };
+export {
+  logoKakao,
+  logoText,
+  profileImg,
+  logoShadow,
+  mascotHappy,
+  logoPig,
+  mascotNone,
+  qrOverlay,
+  logoTest,
+  shadowChar,
+};
