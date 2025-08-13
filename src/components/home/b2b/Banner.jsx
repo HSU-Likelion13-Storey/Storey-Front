8c7e27c4df92741e60a42121ea52bf4ce7322219
조윤지 <yunji8533@naver.com>
2025-08-14 03:06:12 +0900

    ✨: B2BHome 페이지 깜짝이벤트 배너 컴포넌트 구현

--- src/components/home/b2b/Banner.jsx
+++ src/components/home/b2b/Banner.jsx
@@ -1,11 +1,19 @@
+import "./Banner.scss";
+import { IoChevronForward } from "react-icons/io5";
+
 export default function Banner({ title, subtitle, onClick }) {
   return (
     <button className="banner" type="button" onClick={onClick}>
-      <div>
-        <div className="banner-title">{title}</div>
-        <div className="banner-subtitle">{subtitle}</div>
+      <div className="banner__left">
+        <span className="banner__emoji">🎉</span>
+
+        <div className="banner__texts">
+          <div className="banner-title">{title}</div>
+          <div className="banner-subtitle">{subtitle}</div>
+        </div>
       </div>
-      <span className="banner-chevron">›</span>
+
+      <IoChevronForward className="banner-chevron" />
     </button>
   );
 }
