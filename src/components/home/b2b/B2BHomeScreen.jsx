dd7a69a89b2c191d5a6b4f9338f80cf600d7dcd3
조윤지 <yunji8533@naver.com>
2025-08-14 04:56:43 +0900

    💄: B2BHomeScreen 컴포넌트 스타일 구현

--- src/components/home/b2b/B2BHomeScreen.jsx
+++ src/components/home/b2b/B2BHomeScreen.jsx
@@ -3,24 +3,24 @@ import Banner from "./Banner";
 import CharacterBlock from "./CharacterBlock";
 import SummaryBlock from "./SummaryBlock";
 import logo from "../../../assets/logo-text.svg";
+import "./B2BHomeScreen.scss";
 
 export default function B2BHomeScreen() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    // (임시!) TODO: API로 교체
+    // (임시) TODO: API로 교체
     setData({
       banner: { title: "깜짝 이벤트 올리기!", subtitle: "오늘은 햄버거 추천 어때요?" },
       character: {
         speech: "행복한 하루와 위로를 선물해드릴게요.",
         imageSrc: "/src/assets/mascot-happy.svg",
         name: "하루치",
-        description:
-          "하루치는 따뜻하고 말이 느린 아이예요. ‘버거는 패스트푸드가 아니다. 정성이 담긴 슬로우푸드다’가 좌우명이랍니다.",
+        description: `하루치는 따뜻하고 말이 느린 아이에요, ‘버거는 패스트푸드가 아니다. 정성이 담긴 슬로우푸드다'가 좌우명이랍니다.`,
       },
       summary: {
         title: "가게 요약 서사",
-        content: "‘하루 한 끼, 진심으로 위로받는 식사’를 만들기 위해.........",
+        content: `“하루 한 끼, 진심으로 위로받는 식사"를 만들기 위해 퇴사 후 작은 가게를 연 사장님의 이야기가 담겨 있는 캐릭터로 하루 30개 한정의 수제버거, 그리고 ‘고추마요 쉬림프버거’에 담긴 정성이 이 가게의 심장이다.`,
       },
     });
   }, []);
@@ -29,16 +29,8 @@ export default function B2BHomeScreen() {
 
   return (
     <>
-      <header
-        className="b2b-header"
-        style={{
-          paddingTop: "68px",
-          paddingLeft: "30px",
-          paddingBottom: "41px",
-          display: "flex",
-          alignItems: "center",
-        }}>
-        <img src={logo} alt="스토어리 로고" style={{ weight: "89px", height: "29px", objectFit: "contain" }} />
+      <header className="b2b-header">
+        <img src={logo} alt="스토어리 로고" />
       </header>
 
       <main className="b2b-home">
