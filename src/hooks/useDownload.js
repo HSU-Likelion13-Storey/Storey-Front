8be0d4547a6a7a7a4b532a4271bea3fdb96d8078
구혁모 <gurah129@gmail.com>
2025-08-14 03:40:05 +0900

    ⚡️: useDownload 훅 개선

--- src/hooks/useDownload.js
+++ src/hooks/useDownload.js
@@ -6,22 +6,29 @@ export function useDownload(filename = "download.png") {
   const [preview, setPreview] = useState(null); // 캡쳐한 이미지 미리 보여주기
   const ref = useRef(null); // 다운로드할 dom 연결
 
+  const getDataURL = async () => {
+    if (!ref.current) return;
+    const canvas = await html2canvas(ref.current, {
+      useCORS: true,
+      allowTaint: true,
+      width: ref.current.offsetWidth, // 캡쳐할 영역 명시(흰색 선 오류 수정)
+      height: ref.current.offsetHeight, // 캡쳐할 영역 명시
+    });
+    return canvas.toDataURL("image/png");
+  };
+
   // 캡쳐 저장 함수
   const showPreview = async () => {
-    if (!ref.current) return;
-    const canvas = await html2canvas(ref.current);
-    const dataURL = canvas.toDataURL("image/png");
+    const dataURL = await getDataURL();
     setPreview(dataURL);
   };
 
   // 다운로드 함수
   const download = async (preview = null) => {
-    if (!ref.current) return;
-    const canvas = await html2canvas(ref.current);
-    const dataURL = canvas.toDataURL("image/png");
+    const dataURL = await getDataURL();
 
     const link = document.createElement("a");
-    link.href = preview == null ? dataURL : preview;
+    link.href = preview && typeof preview === "string" ? preview : dataURL;
     link.download = filename;
     link.click();
     setDownModal(true);
