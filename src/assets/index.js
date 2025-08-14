a6550f727ff283f3eca8d4ce334a0fb139a2dc01
조윤지 <yunji8533@naver.com>
2025-08-14 18:55:04 +0900

    💄: 임시 캐릭터 이미지 적용

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
-import logoKakao from "./kakao-logo.svg";
 import logoText from "./logo-text.svg";
 import profileImg from "./profile.svg";
 import logoShadow from "./logo-shadow.svg";
 import mascotHappy from "./mascot-happy.svg";
+import testlogo from "./test-character.svg";
 
-export { logoKakao, logoText, profileImg, logoShadow, mascotHappy };
+export { logoText, profileImg, logoShadow, mascotHappy, testlogo };
