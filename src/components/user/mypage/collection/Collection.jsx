d7a0434d7cb007123c8e01f7229c8f9ebc113ec3
구혁모 <gurah129@gmail.com>
2025-08-17 00:50:00 +0900

    💄: 도감 페이지 UI 수정

--- src/components/user/mypage/collection/Collection.jsx
+++ src/components/user/mypage/collection/Collection.jsx
@@ -1,6 +1,7 @@
 import { IoIosArrowBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import "./Collection.scss";
+import { testlogo } from "@/assets";
 
 export const Collection = () => {
   const nav = useNavigate();
@@ -9,13 +10,16 @@ export const Collection = () => {
     <div className="collection">
       <div className="header">
         <IoIosArrowBack className="back-icon" onClick={() => nav(-1)} />
-        <div className="header-title">나의 캐릭터 도감</div>
+        <div className="header-title">내가 모은 캐릭터</div>
         <div className="blank" />
       </div>
-      <div className="lists">
+      <div className="collection-list">
         {mockData.map((data) => (
-          <div className="list-item" key={data.id}>
-            <img src={data.imgUrl} alt="test" />
+          <div className="collection-list-item" key={data.id}>
+            <div className="collection-list-img">
+              <img src={data.imgUrl} alt="test" onClick={() => nav(`/detail/${data.id}`)} />
+            </div>
+            <div className="collection-list-title">{data.title}</div>
           </div>
         ))}
       </div>
@@ -26,54 +30,67 @@ export const Collection = () => {
 const mockData = [
   {
     id: 1,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 2,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼asdadsdd",
   },
   {
     id: 3,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 4,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 5,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 6,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 7,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 8,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 9,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 10,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 11,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 12,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
   {
     id: 13,
-    imgUrl: "test",
+    imgUrl: testlogo,
+    title: "한성돼",
   },
 ];
