c2882bb27de1a10c2f23ca5345e82fd8087a8825
구혁모 <gurah129@gmail.com>
2025-08-16 23:36:26 +0900

    ✨: 수정된 유저 마이페이지로 변경

--- src/components/user/mypage/UserMyPageScreen.jsx
+++ src/components/user/mypage/UserMyPageScreen.jsx
@@ -1,41 +1,33 @@
 import React, { useState } from "react";
-import { IoIosArrowForward } from "react-icons/io";
 import { logoText, profileImg } from "@/assets";
 import { useNavigate } from "react-router-dom";
+import { MyPageListItem } from "@/components/common/mypage/MyPageListItem";
+import { Profile } from "@/components/common/mypage/Profile";
+import "./UserMyPageScreen.scss";
 
 export const UserMyPageScreen = () => {
-  const [role, setRole] = useState("b2b"); // TODO 테스트용 사용자 상태. 추후 보관된 사용자 정보를 불러올 예정.
+  const [collection, setCollection] = useState([]);
   const nav = useNavigate();
+
   return (
     <div className="container">
       {/* Header */}
-      <header className={`header ${role === "user" && "header-user"}`}>
+      <header className="header">
         <img src={logoText} alt="" />
-        <div className="blank"></div>
       </header>
 
       {/* 프로필 카드 */}
-      <div className={`profile-card ${role === "user" && "profile-user"}`}>
-        <div className="profile-content">
-          <img src={profileImg} alt="" />
-          <span>최사장</span>
-        </div>
+      <Profile img={profileImg} name={"김구밍"} id={"Lion1234"} />
+      <div className="collection-button" onClick={() => nav("/mypage/user/collection")}>
+        내가 모은 캐릭터 {collection.length}개
       </div>
 
       {/* 네비게이션, 로그아웃 */}
       <div className="list-content">
-        <ListItem text={"나의 캐릭터 도감"} onClick={() => nav("/mypage/user/collection")} />
-        <ListItem text={"로그아웃"} />
+        <MyPageListItem text={"계정"} />
+        <MyPageListItem text={"고객센터"} />
+        <MyPageListItem text={"로그아웃"} />
       </div>
-    </div>
-  );
-};
-
-const ListItem = ({ children, logout = false, text, onClick }) => {
-  return (
-    <div className={`list-item ${logout && "logout"}`} onClick={onClick}>
-      <span>{text}</span>
-      {children ? children : <IoIosArrowForward className="icon" />}
     </div>
   );
 };
