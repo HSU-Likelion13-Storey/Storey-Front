74cdb5e0feec72cac8f47e00252057e6937974a5
구혁모 <gurah129@gmail.com>
2025-08-16 19:31:22 +0900

    📝: 모달 주석 추가

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import "./Modal.scss";
+
+// 사용할 컴포넌트에서 모달 상태를 관리하고, props로 모달을 닫을 함수를 연결해줘야 함(cancelFn으로)
 export const Modal = ({
-  title,
-  caption,
-  cancel = "",
-  confirm = "",
-  cancelFn = () => {},
-  confirmFn = () => {},
-  confirmType = true,
-  img = "",
-  autoCloseSec = 3,
+  title, // 모달 제목
+  caption, // 모달 설명
+  cancel = "", // 취소 버튼 이름, 없으면 확인 버튼만 있도록 설정
+  confirm = "", // 확인 버튼 이름
+  cancelFn = () => {}, // 취소 버튼 콜백 함수. 닫는 로직도 여기에 담아서 보내줘야 함
+  confirmFn = () => {}, // 확인 버튼 콜백 함수
+  confirmType = true, // 확인 모달인지, 자동 닫힘 모달인지 구분
+  img = "", // 이미지
+  autoCloseSec = 3, // 자동 닫힘 시간
 }) => {
   const [isVisible, setIsVisible] = useState(false); // fade-in, out을 설정할 상태
+
   // 타이머 타입일 경우 3초간 띄우기
   useEffect(() => {
     let timer;
@@ -29,6 +32,7 @@ export const Modal = ({
     setIsVisible(true);
   }, []);
 
+  // 부드러운 모달을 위한 지연시간 추가(언마운트 시 바로 삭제되지 않음)
   const clickHandle = (callback) => {
     setIsVisible(false);
     setTimeout(callback, 300);
