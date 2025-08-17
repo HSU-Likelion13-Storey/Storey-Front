72d73c3d9924bbc2da504cbd96cfcd60f9e5290b
구혁모 <gurah129@gmail.com>
2025-08-18 00:49:25 +0900

    ✨: 스플래쉬 화면 UI 구현

---
+++ src/components/common/splash/Splash.jsx
@@ -0,0 +1,29 @@
+import { mascotBubble } from "@/assets";
+import styles from "./Splash.module.scss";
+import { useState } from "react";
+
+export const Splash = ({ closeFn }) => {
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <div className={`${styles.container} ${visible && styles.visible}`}>
+      <div className={styles.content}>
+        <div className={styles.img}>
+          <img src={mascotBubble} />
+        </div>
+        <div className={styles.title}>
+          <h1>스토어리</h1>
+          <span className={styles.caption}>가게 스토리, 캐릭터가 되다.</span>
+        </div>
+        <div
+          className={styles.button}
+          onClick={() => {
+            setVisible(false);
+            closeFn();
+          }}>
+          시작하기
+        </div>
+      </div>
+    </div>
+  );
+};
