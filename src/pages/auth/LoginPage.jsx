d32826b02ee55e6978893870c60724e95d0e6abb
구혁모 <gurah129@gmail.com>
2025-08-18 01:07:32 +0900

    ✨: 로그인, 홈페이지 스플래시 화면 적용

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import LoginScreen from "../../components/auth/login/LoginScreen";
+import { Splash } from "@/components/common/splash/Splash";
+import { useSplash } from "@/hooks/useSplash";
 
 const LoginPage = () => {
+  const { isSplash, handleSplash } = useSplash();
+
   return (
     <div>
+      {!isSplash && <Splash closeFn={handleSplash} />}
       <LoginScreen />
     </div>
   );
--- src/pages/b2b/B2BHomePage.jsx
+++ src/pages/b2b/B2BHomePage.jsx
@@ -1,8 +1,13 @@
 import B2BHomeScreen from "@/components/b2b/home/B2BHomeScreen";
+import { Splash } from "@/components/common/splash/Splash";
+import { useSplash } from "@/hooks/useSplash";
 
 export default function B2BHome() {
+  const { isSplash, handleSplash } = useSplash();
+
   return (
     <>
+      {!isSplash && <Splash closeFn={handleSplash} />}
       <B2BHomeScreen />
     </>
   );
