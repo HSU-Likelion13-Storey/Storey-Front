15fe643234f36b5dd6476446fbfdaac7b182618b
구혁모 <gurah129@gmail.com>
2025-08-18 01:26:54 +0900

    🚑: 병합 충돌 수정

--- src/pages/b2b/B2BHomePage.jsx
+++ src/pages/b2b/B2BHomePage.jsx
@@ -2,7 +2,7 @@ import B2BHomeScreen from "@/components/b2b/home/B2BHomeScreen";
 import { Splash } from "@/components/common/splash/Splash";
 import { useSplash } from "@/hooks/useSplash";
 
-export default function B2BHome() {
+export default function B2BHomePage() {
   const { isSplash, handleSplash } = useSplash();
 
   return (
