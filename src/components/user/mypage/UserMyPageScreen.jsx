2e2bc9c74ed83fec2fc3546f7340be6bb6604502
구혁모 <gurah129@gmail.com>
2025-08-21 22:32:12 +0900

    ✨: 유저 마이페이지 API 연결(프로필, 도감 개수)

--- src/components/user/mypage/UserMyPageScreen.jsx
+++ src/components/user/mypage/UserMyPageScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { logoText, profileImg } from "@/assets";
 import { useNavigate } from "react-router-dom";
 import { MyPageListItem } from "@/components/common/mypage/MyPageListItem";
@@ -9,7 +9,11 @@ import api from "@/apis/Instance";
 import { useAuthStore } from "@/store/useAuthStore";
 
 export const UserMyPageScreen = () => {
-  const [collection, setCollection] = useState([]);
+  const [collection, setCollection] = useState(0);
+  const [profile, setProfile] = useState({
+    nickName: "김구밍",
+    loginId: "lion1234",
+  });
   const [logoutModal, setLogoutModal] = useState(false);
   const nav = useNavigate();
   const { logout } = useAuthStore();
@@ -27,6 +31,27 @@ export const UserMyPageScreen = () => {
     }
   };
 
+  useEffect(() => {
+    const getCollection = async () => {
+      try {
+        const res = await api.get("user/collection");
+        if (res.data.isSuccess) setCollection(res.data.data.totalCount);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const getProfile = async () => {
+      try {
+        const res = await api.get("user/mypage");
+        if (res.data.isSuccess) setProfile(res.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCollection();
+    getProfile();
+  }, []);
+
   return (
     <div className="container">
       {/* Header */}
@@ -35,9 +60,9 @@ export const UserMyPageScreen = () => {
       </header>
 
       {/* 프로필 카드 */}
-      <Profile img={profileImg} name={"김구밍"} id={"Lion1234"} />
+      <Profile img={profileImg} name={profile.nickName} id={profile.loginId} />
       <div className="collection-button" onClick={() => nav("/mypage/user/collection")}>
-        내가 모은 캐릭터 {collection.length}개
+        내가 모은 캐릭터 {collection}개
       </div>
 
       {/* 네비게이션, 로그아웃 */}
