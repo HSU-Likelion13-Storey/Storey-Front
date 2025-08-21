7b98e989b5de29670ac049f9a3c3d0dc111a2c85
구혁모 <gurah129@gmail.com>
2025-08-22 01:10:31 +0900

    ✨: 캐릭터 상세보기 API 연결 및 매핑

--- src/components/user/character-detail/CharacterDetail.jsx
+++ src/components/user/character-detail/CharacterDetail.jsx
@@ -1,3 +1,4 @@
+import api from "@/apis/Instance";
 import styles from "./CharacterDetail.module.scss";
 import { logoTest, shadowChar } from "@/assets";
 import { Modal } from "@/components/common/Modal";
@@ -6,29 +7,50 @@ import { useEffect, useState } from "react";
 import { FiDownload } from "react-icons/fi";
 import { HiCamera } from "react-icons/hi2";
 import { IoIosArrowBack } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const mock = {
-  title: "참새방앗간",
-  address: "서울 성북구 삼선교로 11길 20 1층",
-  message: "“들어왔으면, 한 잔부터 받아요. 오늘은 어땠어요?”",
-  img: logoTest,
+  characterId: 1,
+  storeName: "참새방앗간",
+  addressMain: "서울 성북구 삼선교로 11길 20",
+  addressDetail: "1층",
+  tagline: "“들어왔으면, 한 잔부터 받아요. 오늘은 어땠어요?”",
+  imageUrl: logoTest,
   name: "짹이",
-  caption: "수다쟁이 & 다정다감하고, ‘하루쯤은 떠들고 웃고 맛있는 걸 마셔야지.’가 좌우명이다.",
-  story:
+  description: "수다쟁이 & 다정다감하고, ‘하루쯤은 떠들고 웃고 맛있는 걸 마셔야지.’가 좌우명이다.",
+  narrativeSummary:
     " “하루 한 끼, 진심으로 위로받는 식사”를 만들기 위해 퇴사 후 작은 가게를 연 사장님의 이야기가 담겨 있는 캐릭터로 하루 30개 한정의 수제버거, 그리고 ‘고추마요 쉬림프버거’에 담긴 정성이 이 가게의 심장이다.",
 };
 
 export const CharacterDetail = () => {
-  const [detail, setDetail] = useState({});
+  const [detail, setDetail] = useState({
+    characterId: null,
+    imageUrl: "",
+    tagline: "",
+    name: "",
+    description: "",
+    narrativeSummary: "",
+    storeName: "",
+    addressMain: "",
+    addressDetail: "",
+  });
   const [modalOpen, setModalOpen] = useState(true);
   const { ref, download, setDownModal, downModal } = useDownload("test.png");
   const nav = useNavigate();
+  const { id } = useParams();
 
   useEffect(() => {
-    // TODO api 통신
-    setDetail(mock);
-  }, []);
+    const getDetailData = async () => {
+      try {
+        const res = await api.get(`user/characters/${id}`);
+        if (res.data.isSuccess) setDetail(res.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    // setDetail(mock);
+    getDetailData();
+  }, [id]);
 
   return (
     <div className={styles.container}>
@@ -36,25 +58,27 @@ export const CharacterDetail = () => {
       <div className={styles.header}>
         <IoIosArrowBack className={styles.icon} onClick={() => nav(-1)} />
         <span className={styles.headerTitle}>내가 모은 캐릭터</span>
-        <HiCamera className={styles.iconCamera} onClick={() => nav("/capture", { state: mock })} />
+        <HiCamera className={styles.iconCamera} onClick={() => nav("/capture", { state: detail })} />
       </div>
 
       {/* 메인 콘텐츠(제목, 카드, 다운로드) */}
       <div className={styles.content}>
         <div className={styles.title}>
-          <span className={styles.headline}>{detail.title}</span>
-          <span className={styles.address}>{detail.address}</span>
+          <span className={styles.headline}>{detail.storeName}</span>
+          <span className={styles.address}>
+            {detail.addressMain} {detail.addressDetail}
+          </span>
         </div>
         <div className={styles.card} ref={ref}>
           <div className={styles.message}>
-            <span>{detail.message}</span>
+            <span>{detail.tagline}</span>
           </div>
           <div className={styles.character}>
-            <img src={detail.img} className={styles.char} alt="" />
+            <img src={detail.imageUrl} className={styles.char} alt="" />
             <img src={shadowChar} className={styles.charShadow} alt="" />
           </div>
           <span className={styles.name}>{detail.name}</span>
-          <span className={styles.caption}>{detail.caption}</span>
+          <span className={styles.caption}>{detail.description}</span>
         </div>
         <div className={styles.download} onClick={download}>
           <FiDownload className={styles.icon} />
@@ -65,7 +89,7 @@ export const CharacterDetail = () => {
       {/* 가게 요약 서사 */}
       <div className={styles.summary}>
         <span className={styles.summaryTitle}>가게 요약 서사</span>
-        <div className={styles.summaryText}>{detail.story}</div>
+        <div className={styles.summaryText}>{detail.narrativeSummary}</div>
       </div>
       {modalOpen && (
         <Modal
