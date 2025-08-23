9f80739ec3b1b080c1d83fdd69dce22110e3fc45
조윤지 <yunji8533@naver.com>
2025-08-24 02:22:43 +0900

    ✨: 캐릭터 재생성 API 추가

--- src/apis/chatbot/ownerCharacterApi.js
+++ src/apis/chatbot/ownerCharacterApi.js
@@ -1,7 +1,14 @@
 // 챗봇 캐릭터 생성 결과 확인 API(POST)
 import api from "../Instance";
 
+// 캐릭터 생성 결과 확인
 export const confirmOwnerCharacter = async (answer) => {
   const { data } = await api.post("/owner/character", { answer });
+  return data;
+};
+
+// 캐릭터 재생성 (다시 만들래요 버튼)
+export const regenerateOwnerCharacter = async (answer) => {
+  const { data } = await api.put("/owner/character", { answer });
   return data;
 };
