e7aed453763111a9cddc2f3f433b9345e6dbcab9
조윤지 <yunji8533@naver.com>
2025-08-24 22:19:31 +0900

    ✨: 내 캐릭터 조회 API 생성 추가 (로그인 후 분기 시 필요)

--- src/apis/character/characterApi.js
+++ src/apis/character/characterApi.js
@@ -27,3 +27,17 @@ export const updateCharacter = async (characterId, payload) => {
     return null;
   }
 };
+
+// 내 캐릭터 조회 API(GET)
+export const getMyCharacter = async () => {
+  try {
+    const res = await api.get("/owner/character");
+    if (res.data?.isSuccess) {
+      return res.data.data;
+    }
+    return null;
+  } catch (err) {
+    console.error("내 캐릭터 조회 실패:", err);
+    return null;
+  }
+};
