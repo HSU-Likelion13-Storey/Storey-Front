4d7b00604244e3ea2bad37fd5b7d0abc61f6d845
조윤지 <yunji8533@naver.com>
2025-08-24 18:47:03 +0900

    ✨: 캐릭터 생성완료 페이지에 캐릭터 이미지 반영

--- src/components/b2b/chatbot/CharacterComplete.jsx
+++ src/components/b2b/chatbot/CharacterComplete.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BrandHeading from "@/components/auth/signup/common/BrandHeading.jsx";
 import Button from "@/components/auth/signup/common/Button.jsx";
-import { testlogo } from "@/assets";
 import "./CharacterComplete.scss";
+
+import { useAuthStore } from "@/store/useAuthStore";
+import { getCharacter } from "@/apis/character/characterApi";
 
 export default function CharacterComplete() {
   const nav = useNavigate();
+  const { characterId } = useAuthStore();
+  const [character, setCharacter] = useState(null);
+
+  // 캐릭터 조회
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      if (!characterId) return;
+      const char = await getCharacter(characterId);
+      if (char) setCharacter(char);
+    };
+    fetchCharacter();
+  }, [characterId]);
+
   return (
     <div className="character-complete-container">
       <BrandHeading title={"우리 가게 마스코트\n생성 완료!"} />
@@ -14,7 +29,11 @@ export default function CharacterComplete() {
 
       <div className="character-complete">
         <div className="speech">함께하게 되어 영광이에요!</div>
-        <img className="character" src={testlogo} alt="가게 마스코트" />
+        {character ? (
+          <img className="character" src={character.imageUrl} alt="가게 마스코트" />
+        ) : (
+          <div className="character-placeholder">캐릭터 불러오는 중...</div>
+        )}{" "}
         <Button type="button" onClick={() => nav("/home/owner")} className="start-btn btn-enabled">
           가봅시다!
         </Button>
