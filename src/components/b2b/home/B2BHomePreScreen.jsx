62bc72e76c5a6c815d9418d26888c9f3076b664c
구혁모 <gurah129@gmail.com>
2025-08-25 17:26:40 +0900

    ✨: 사장 홈페이지 가이드 UI 제작

--- src/components/b2b/home/B2BHomePreScreen.jsx
+++ src/components/b2b/home/B2BHomePreScreen.jsx
@@ -1,14 +1,16 @@
 // src/components/b2b/home/B2BHomePreScreen.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Banner from "./Banner";
 import EmptyStateCard from "./EmptyStateCard";
 import logo from "../../../assets/logo-text.svg";
 import questionIcon from "../../../assets/questionIcon.svg";
 import "./B2BHomePreScreen.scss";
+import { B2BHomePreGuideOverlay } from "./B2BHomePreGuideOverlay";
 
 export default function B2BHomePreScreen() {
   const nav = useNavigate();
+  const [visible, setVisible] = useState(true);
 
   const bannerData = {
     title: "깜짝 이벤트 올리기!",
@@ -16,8 +18,14 @@ export default function B2BHomePreScreen() {
     onClick: () => nav("/home/owner/event"),
   };
 
+  useEffect(() => {
+    const isGuideClose = localStorage.getItem("pre-guide-close");
+    if (isGuideClose) setVisible(false);
+  }, []);
+
   return (
     <>
+      {visible && <B2BHomePreGuideOverlay setVisible={setVisible} visible={visible} />}
       <header className="b2b-header">
         <img src={logo} alt="스토어리 로고" />
       </header>
